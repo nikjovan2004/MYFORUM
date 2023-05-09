53d323de9891133c25fb539b8dd5edae01ed6df4
Nik <nik.jovan1@gmail.com>
2023-05-09 20:41:11 +0200

    tinymce

---
+++ app/assets/javascripts/tinymce_init.js
@@ -0,0 +1,16 @@
+tinymce.init({
+    selector: 'textarea',
+    height: 300,
+    menubar: false,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table contextmenu paste code help'
+    ],
+    toolbar: 'undo redo | formatselect | ' +
+             'bold italic backcolor | alignleft aligncenter ' +
+             'alignright alignjustify | bullist numlist outdent indent | ' +
+             'removeformat | help',
+    content_css: '//www.tiny.cloud/css/codepen.min.css'
+  });
+  
