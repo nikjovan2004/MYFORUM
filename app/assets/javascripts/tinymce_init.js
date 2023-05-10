12422154e3b9fe802563e7ebed66d28e7f3c414e
Nik <nik.jovan1@gmail.com>
2023-05-10 16:13:44 +0200

    tinymceinit_changes

--- app/assets/javascripts/tinymce_init.js
+++ app/assets/javascripts/tinymce_init.js
@@ -1,16 +1,17 @@
 tinymce.init({
-    selector: 'textarea',
+    content_css: "//www.tiny.cloud/css/codepen.min.css",
     height: 300,
     menubar: false,
+    selector: "textarea",
     plugins: [
-      'advlist autolink lists link image charmap print preview anchor',
-      'searchreplace visualblocks code fullscreen',
-      'insertdatetime media table contextmenu paste code help'
+      "advlist autolink lists link image charmap print preview anchor",
+      "searchreplace visualblocks code fullscreen",
+      "insertdatetime media table contextmenu paste code help"
     ],
-    toolbar: 'undo redo | formatselect | ' +
-             'bold italic backcolor | alignleft aligncenter ' +
-             'alignright alignjustify | bullist numlist outdent indent | ' +
-             'removeformat | help',
-    content_css: '//www.tiny.cloud/css/codepen.min.css'
+    toolbar: "undo redo | formatselect | " +
+             "bold italic backcolor | alignleft aligncenter " +
+             "alignright alignjustify | bullist numlist outdent indent | " +
+             "removeformat | help"
+
   });
   
